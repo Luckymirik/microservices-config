3b47657e35c4893b6de22a379468047e5dcbacf2
Miroslav Tsadkin <Luckymirik@mail.ru>
2026-04-29 13:45:58 +0300

    stabilize microservices infrastructure (docker, eureka, config-server, gateway)

--- company-service/src/test/java/com/example/companyservice/CompanyServiceApplicationTests.java
+++ company-service/src/test/java/com/example/companyservice/CompanyServiceApplicationTests.java
@@ -3,11 +3,15 @@ package com.example.companyservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+        properties = {
+                "spring.autoconfigure.exclude=" +
+                        "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+        }
+)
 class CompanyServiceApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
